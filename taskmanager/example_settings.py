1164839b93f123adf6804d60e59c366205dd2859
nina20126 <nina20126@student.hamk.fi>
2023-11-30 23:52:35 +0200

    first version of user profile with name and picture

--- taskmanager/example_settings.py
+++ taskmanager/example_settings.py
@@ -16,6 +16,7 @@ Example settings.py file. Adding your settings.py to .gitignore, as it will cont
 """
 
 from pathlib import Path
+import os 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -118,11 +119,17 @@ USE_I18N = True
 
 USE_TZ = True
 
+LOGIN_URL = 'login'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import RegisterPage, CustomLoginView
 from django.contrib.auth.views import LogoutView
+from users import views as user_views
 
 urlpatterns = [
     path('register/', RegisterPage.as_view(), name='register'),
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('profile/', user_views.profile, name='profile')
 
 ]
