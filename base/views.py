976f88ac2e81d2ba72a88d4e9002de650d04b2db
Apollostowel <apollostowel@icloud.com>
2023-12-02 18:39:16 +0200

    converted to task

--- base/views.py
+++ base/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render, redirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
+
 from django.urls import reverse_lazy
 
 from django.contrib.auth.views import LoginView 
